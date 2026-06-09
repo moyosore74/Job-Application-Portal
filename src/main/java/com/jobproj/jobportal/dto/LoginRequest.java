2d4e0590bf772da5976e6bf6c81f5d0cb27df04b
Ogunwoolu Abdulmatin <ogunwoolua@gmail.com>
2026-06-09 12:27:58 +0100

    Tighten login request validation

--- src/main/java/com/jobproj/jobportal/dto/LoginRequest.java
+++ src/main/java/com/jobproj/jobportal/dto/LoginRequest.java
@@ -2,14 +2,17 @@ package com.jobproj.jobportal.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class LoginRequest {
 
     @Email
     @NotBlank
+    @Size(max = 150)
     private String email;
 
     @NotBlank
+    @Size(max = 72)
     private String password;
 
     public String getEmail() {
