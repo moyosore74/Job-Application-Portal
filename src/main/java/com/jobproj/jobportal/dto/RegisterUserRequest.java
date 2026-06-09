bda70ae604a970a35ca83a6858ac5317e0216d9f
Ogunwoolu Abdulmatin <ogunwoolua@gmail.com>
2026-06-09 12:27:14 +0100

    Tighten auth request validation

--- src/main/java/com/jobproj/jobportal/dto/RegisterUserRequest.java
+++ src/main/java/com/jobproj/jobportal/dto/RegisterUserRequest.java
@@ -4,23 +4,30 @@ import com.jobproj.jobportal.entity.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class RegisterUserRequest {
 
     @NotBlank
+    @Size(max = 150)
     private String fullName;
 
     @Email
     @NotBlank
+    @Size(max = 150)
     private String email;
 
     @NotBlank
+    @Size(min = 8, max = 72)
     private String password;
 
     @NotNull
     private Role role;
 
+    @Size(max = 50)
     private String companyId;
+
+    @Size(max = 150)
     private String companyName;
 
     public String getFullName() {
